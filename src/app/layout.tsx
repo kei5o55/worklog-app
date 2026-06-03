31b52c590895f27ba67fba9d67bbbd3e471c1f09
kei5o55 <uttk2003@gmail.com>
2026-06-04 02:52:13 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,8 @@ import "./globals.css";
 
 export default function RootLayout({ children }: {children: React.ReactNode}){
     return (
-    
         <html lang="ja">
-            
             <body>
-                <p>これがlayout.tsxだ</p>
                 {children}
             </body>
         </html>
